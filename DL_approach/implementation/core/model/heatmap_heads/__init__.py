63e5dd966875eb49697a32668b586f26770d4b8d
atlas <atlas0435@gmail.com>
2022-07-12 22:54:38 +0800

    add new heatmap head

--- DL_approach/implementation/core/model/heatmap_heads/__init__.py
+++ DL_approach/implementation/core/model/heatmap_heads/__init__.py
@@ -2,3 +2,6 @@ from . import CustomV1
 from . import CustomV2
 from . import CustomV2_mish
 from . import CustomV3
+from . import CustomV3_expand
+from . import CustomV3_reverse
+from . import CustomV3_reverse_expand
