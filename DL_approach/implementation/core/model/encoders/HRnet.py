6713931927ac8d122033ec6d0642df1af89b08ed
atlas <atlas0435@gmail.com>
2022-07-19 12:13:02 +0800

    add new encoder

--- DL_approach/implementation/core/model/encoders/HRnet.py
+++ DL_approach/implementation/core/model/encoders/HRnet.py
@@ -3,7 +3,7 @@ from .base import BaseEncoder
 
 class Encoder(BaseEncoder):
     def __init__(self,subtype):
-        encoder = timm.create_model('convnext_'+subtype, pretrained=True, features_only=True)
+        encoder = timm.create_model('hrnet_'+subtype, pretrained=True, features_only=True)
         out_channels = encoder.feature_info.channels()
         super().__init__(encoder,out_channels)
         
